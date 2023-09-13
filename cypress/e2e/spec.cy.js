57a49a31e3a2a3150fcba8e6c5b5c113882aebc5
EstefaniaAguirre13 <edurango@netgate.com>
2023-09-13 16:02:50 -0500

    corregir generacion de reportes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,13 @@
+import { LoginMethods } from "./pages/login/login.methods";
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    const usuario = 'random01';
+    const contrasena = 'random01'
+    cy.visit('https://www.demoblaze.com/index.html')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethods.login(usuario, contrasena)
+    cy.get('a#nameofuser').should('contain.te', '')
+    cy.wait(500000)
   })
 })
