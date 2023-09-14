e7dba1f20b59fc0584989d92c1063679527fc6bc
EstefaniaAguirre13 <edurango@netgate.com>
2023-09-14 10:43:11 -0500

    Creacion de la pagina place order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData{
+    static get testDate(){
+        return {
+            name: 'Teste name',
+            country: 'United States',
+            city: 'Boston',
+            creditCard: '40128888888881881',
+            month: 12,
+            year: 2024
+        }
+    }
+}
